f9cd5a6037031e88f7a66a95170595e101159273
Rajat Gangwar <rajat.gangwar41@gmail.com>
2022-08-02 02:08:17 +0530

    backend setup and running test

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan("dev"));
+
+//routes
+app.get("/",(req,res) => {
+      res.send("Mcbucks testing server by Rajat Gangwar");
+});
+
+//Create Port 
+const PORT = process.env.PORT || 5000;
+
+//Listen
+app.listen(PORT, () => {
+    console.log(`Serve at running on the port: http://localhost:${PORT}`);
+} )
