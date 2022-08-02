f860210fe2bb5cbd942d57df66df2566e29140fa
Rajat Gangwar <rajat.gangwar41@gmail.com>
2022-08-03 01:31:11 +0530

    test

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
+//Connect with MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log("Connected to DB");
+}).catch((err) => {
+    console.log(err.message);
+});
+
 const app = express();
 
 //middlewares
