69ebdd30448298f7ba0d5b6f182c09ae18100d1f
ShivamYadav0207 <shivamyadav.0207@gmail.com>
2022-08-04 01:30:07 +0530

    ---

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import 'antd/dist/antd.min.css';
 import './App.css';
 import Home from './pages/home/Home';
 import Products from './pages/products/Products';
+import Cart from './pages/cart/Cart';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/products" element={<Products/>} />
+          <Route path="/" element={
+            <ProtectedRouter>
+              <Home />
+            </ProtectedRouter>
+            } />
+          <Route path="/products" element={
+            <ProtectedRouter>
+              <Products />
+            </ProtectedRouter>
+            } />
+          <Route path="/cart" element={
+            <ProtectedRouter>
+              <Cart />
+            </ProtectedRouter>
+            } />
         </Routes>
       </Router>
     </>
   );
 }
 
-export default App
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store = {store}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
