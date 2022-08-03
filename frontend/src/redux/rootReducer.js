47708b606346310a57cbd8762ddea4e6bd488b81
ShivamYadav0207 <shivamyadav.0207@gmail.com>
2022-08-04 00:42:43 +0530

    rootReducer.js added

---
+++ frontend/src/redux/rootReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    loading: false,
+    cartItems: []
+}
+
+export const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default: return state;
+    }
+}
