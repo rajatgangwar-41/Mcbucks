f5650da2f35f2ae8fc7c5f32bce1dba7d974aae7
Rajat Gangwar <rajat.gangwar41@gmail.com>
2022-08-04 01:36:02 +0530

    updateProductController

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getProductController, addProductController } from "../controllers/productController.js";
+import { getProductController, addProductController, updateProductController } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.get("/getproducts", getProductController);
 
 productRouter.post("/addproducts", addProductController);
+
+productRouter.put("/updateproducts", updateProductController);
 
 export default productRouter;
